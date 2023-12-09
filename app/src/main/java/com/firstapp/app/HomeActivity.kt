037478eee01b25dc013de94865ae8481b1b8545d
HoudaMounir <mounir.houda02@gmail.com>
2023-12-09 11:55:11 +0100

    HomeActivity XML FILE_modified

--- app/src/main/java/com/firstapp/app/HomeActivity.kt
+++ app/src/main/java/com/firstapp/app/HomeActivity.kt
@@ -1,26 +1,44 @@
 package com.firstapp.app
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    private var drawerLayout: DrawerLayout? = null
+    private var navigationView: NavigationView? = null
+
+    private var drawerToggle: ActionBarDrawerToggle? = null
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle?.onOptionsItemSelected(item) == true) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val bottomNavBar = findViewById<ChipNavigationBar>(R.id.BottomNavBar)
-        bottomNavBar.setOnItemSelectedListener { id ->
-            when (id) {
-                R.id.nav_shopping -> {
-                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.in/Gym-Equipment/s?k=Gym+Equipment"))
-                    startActivity(intent)
-                    true
-                }
-                else -> false
-            }
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout?.addDrawerListener(drawerToggle!!)
+        drawerToggle?.syncState()
+
+    }
+    override fun onBackPressed() {
+        if (drawerLayout?.isDrawerOpen(GravityCompat.START) == true) {
+            drawerLayout?.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
