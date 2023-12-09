e37cec379cb22f80bd015a2fa31d9b2578c26837
HoudaMounir <mounir.houda02@gmail.com>
2023-12-09 13:16:49 +0100

    ShoppingActivity FILES_DONE

---
+++ app/src/main/java/com/firstapp/app/ShppingActivity.kt
@@ -0,0 +1,19 @@
+package com.firstapp.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class ShppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shpping)
+
+
+        val webView = findViewById<WebView>(R.id.web_shop)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.amazon.in/Gym-Equipment/s?k=Gym+Equipment")
+    }
+}
